940ae877c7b236bc55bb5e9a6f6131ffcec360a9
jtaren <arenjatau@gmail.com>
2024-01-29 23:35:16 +0100

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, dafault=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route('/')
 def index():
